1bbc9aa0025bc335b8e8768467e33602c7677a87
feelsuegood <suaj2021@gmail.com>
2023-09-04 22:26:28 +1000

    developing data structure

--- routes/test.js
+++ routes/test.js
@@ -5,23 +5,36 @@ require("dotenv").config();
 /* GET test page. */
 router.get("/", async function (req, res, next) {
   try {
-    console.log(req.query);
+    // Get animal name and threat from GBIF API
     const { name } = req.query;
-
-    // console.log(req.body)
-    // const name = req.body.name;
-    const response = await fetch(
-      `https://api.gbif.org/v1/species/search?q=${name}&threat=CRITICALLY_ENDANGERED`
-    );
-    if (!response.ok) {
+    const gbifAPIUrl = `https://api.gbif.org/v1/species/search?q=${name}&threat=CRITICALLY_ENDANGERED`;
+    const gbifResponse = await fetch(gbifAPIUrl);
+    if (!gbifResponse.ok) {
       throw new Error("Failed to fetch data from GBIF API");
     }
 
-    const data = await response.json();
-    const animals = data.results[0];
+    const gbifData = await gbifResponse.json();
+
+    const animals = gbifData.results[0];
 
     const nameData = [];
     const threatData = [];
+
+    for (let i = 0; i < animals.vernacularNames.length; i++) {
+      const name = animals.vernacularNames[i].vernacularName;
+      nameData.push({ name });
+    }
+    // nameData Json: [
+    //   { name: 'Southwest Bornean Orangutan' },
+    //   { name: 'Central Bornean Orangutan' },
+    //   { name: 'Southwest Bornean Orangutan' },
+    //   { name: 'Central Bornean orangutan' }
+    // ]
+    // // 3번째 항목 호출
+    // const thirdItem = data[2];
+
+    // // 3번째 항목의 "name" 속성 출력
+    // console.log(thirdItem.name);
 
     for (let i = 0; i < animals.threatStatuses.length; i++) {
       // eg. CRITICALLY ENDANGERED
@@ -31,17 +44,53 @@ router.get("/", async function (req, res, next) {
       // animals.threatStatuses[i].replace("_", " ").slice(1).toLowerCase();
       threatData.push({ threat });
     }
+    // threatData Json: [ { threat: 'CRITICALLY ENDANGERED' } ]
 
-    for (let i = 0; i < animals.vernacularNames.length; i++) {
-      const name = animals.vernacularNames[i].vernacularName;
-      nameData.push({ name });
+    //Get animal pictures from Flickr API
+    const FLICKR_API_KEY = process.env.FLICKR_API_KEY;
+    const flickrResponse = await fetch(
+      `https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${FLICKR_API_KEY}&tags=${name}&per-page=50&format=json&nojsoncallback=1&media=photos`
+    );
+
+    if (!flickrResponse.ok) {
+      throw new Error("Failed to fetch data from Flickr API");
     }
 
-    console.log(nameData, threatData);
-    res.render("test", { threatData, nameData });
+    const flickrData = await flickrResponse.json();
+    const photos = flickrData.photos;
+
+    photoData = [];
+
+    const photo = photos.photo[0];
+    const image = `http://farm${photo.farm}.static.flickr.com/${photo.server}/${photo.id}_${photo.secret}_t.jpg`;
+    const url = `http://www.flickr.com/photos/${photo.owner}/${photo.id}`;
+    const title = photo.title;
+    photoData.push({ image, url, title });
+    //     phtoData Json: [
+    //   {
+    //     image: 'http://farm66.static.flickr.com/65535/53157185002_d060f17597_t.jpg',
+    //     url: 'http://www.flickr.com/photos/62428675@N03/53157185002',
+    //     title: 'Cell Service Orangutan'
+    //   }
+    // ]
+    // "url" 속성 호출
+    // const url = data[0].url;
+    // "url" 값 출력
+    // console.log(url);
+
+    // Data fetch test
+    console.log(nameData, threatData, photoData);
+
+    // Lastly render test page
+    res.render("test", { threatData, nameData, photoData });
   } catch (error) {
     console.error("Error fetching data:", error);
-    res.status(500).send("Error fetching data from GBIF API");
+    const title = "Error: Nothing Found";
+    res.status(500).render("error", {
+      title,
+      message: "Nothing Found",
+      description: "Error fetching data",
+    });
   }
 });
 
